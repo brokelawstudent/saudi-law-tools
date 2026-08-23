a4c83b1c6f423434ddd28a4b32305931fadab47e
brokelawstudent <no8fsss@gmail.com>
2026-08-23 23:01:45 +0300

    Export document parser

--- saudi_law_tools/__init__.py
+++ saudi_law_tools/__init__.py
@@ -1,5 +1,7 @@
 from .parser import parse_article
 from .structure import parse_heading
+from .document import parse_document
 
-__all__ = ["parse_article", "parse_heading"]
+__all__ = ["parse_article", "parse_heading", "parse_document"]
+
 
