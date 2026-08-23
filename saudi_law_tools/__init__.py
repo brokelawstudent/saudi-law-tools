536bd9be63a6a7484e728b5a3ebacabc9f0f0cfa
brokelawstudent <no8fsss@gmail.com>
2026-08-23 23:15:15 +0300

    Export normalization utilities

--- saudi_law_tools/__init__.py
+++ saudi_law_tools/__init__.py
@@ -1,7 +1,15 @@
 from .parser import parse_article
 from .structure import parse_heading
 from .document import parse_document
+from .normalize import normalize_arabic_text, normalize_digits
 
-__all__ = ["parse_article", "parse_heading", "parse_document"]
+__all__ = [
+    "parse_article",
+    "parse_heading",
+    "parse_document",
+    "normalize_arabic_text",
+    "normalize_digits",
+]
+
 
 
